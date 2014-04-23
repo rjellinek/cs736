5042728bd4d69a558eade9818d138f2be1bfa4a7
Robert Jellinek <robert.jellinek@gmail.com>
2014-04-22 19:53:49 -0500

    just need to finish printing correctly

--- finalProj/parse_logs.py
+++ finalProj/parse_logs.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import re
-import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import urllib2
 from lxml import etree
 
@@ -17,36 +17,68 @@ class Entry():
   Represents an entry in a changelog file
   '''
   # xpath selector for first link in gmane search response
-  self.gmane_xpath = '//*[@id="mform"]/dl[1]/dt/b/a'
+  gmane_xpath = '//*[@id="mform"]/dl[1]/dt/b/a'
 
   def __init__(self, entry_lines):
     self.add_entry(entry_lines)
-  
+ 
+  def __str__(self):
+    #return ('%s\n\t<a href="%s">%s</a>\n\t%s\n\t%s\n\n') % (self.email,
+    #  self.address, self.title, self.target_files, self.body)
+    return ('<a href="%s">%s</a>%s\t%s\t' + '\t'*10) % (self.address,
+    self.title, self.target_files[0], self.body)
+
   def add_entry(self, entry_lines):
     self.email = entry_lines[0]
     self.title = entry_lines[1]
-    self.body  = entry_lines[2:]
+    self.body  = ' '.join(entry_lines[2:])
+    self.body = re.sub('\s+', ' ', self.body)
+    self.address = ''
+    self.get_address()
+    self.target_page = self.get_comment_page()
+    self.get_target_file(self.target_page)
 
   def get_address(self):
-    try:
-      query = GMANE_SEARCH % urllib2.quote(self.title)
-      response = urllib2.urlopen(url)
-      htmlparser = etree.HTMLParser()
-      tree = etree.parse(response, htmlparser)
-      elem = tree.xpath(xpathselector)
-      link = elem[0].attrib['href']
-      self.address = link
-      return true
-    except Exception as e:
-      print 'Error getting address for patch %s: %s' % (self.title, e)
-      return false
+    if not hasattr(self, 'email'):
+      print 'Error, have not created Entry yet!'
+      return False
+    
+    url = GMANE_SEARCH % urllib2.quote(self.title)
+    response = urllib2.urlopen(url)
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(response, htmlparser)
+    elem = tree.xpath(self.gmane_xpath)
+    link = elem[0].attrib['href']
+    self.address = link
+    return True
 
+  def get_comment_page(self):
+    '''
+    returns comment page (from self.address) 
+    in text format
+    '''
+    #TODO: error checking and retries!
+    # If self.address isn't here, retry self.get_address
+    url = self.address
+    response = urllib2.urlopen(url)
+    html = response.read()
+    bs = BeautifulSoup(html)
+    return bs.text
+  
+  def get_target_file(self, comment_page):
+    '''
+    Takes comment page in text format, saves file edited.
+    '''
+    self.num_target_files = re.findall('(\d+) files changed', comment_page)
+    self.target_files = re.findall('--- a(\S+)\s', comment_page)
+    
+  
 def line_is_start(line):
   '''
   determine starting line of changelog patch by finding email addr
   in std format
   '''
-  return len(re.findall('<*@*.*>', line)) > 0
+  return len(re.findall('^<\S+@\S+\.\S+>', line)) > 0
 
 def parse_changelog_entries(lines):
   '''
@@ -58,9 +90,16 @@ def parse_changelog_entries(lines):
  
   # Add entries
   for i in range(len(lines)):
+    print 'line %d: %s' % (i, lines[i])
     if line_is_start(lines[i]):
+      print 'line is start line, start=%d' % i
       if start > 0:
+        print 'appending entry of lines %d..%d' % (start, i)
         entries.append(Entry(lines[start:i]))
+            
+        ##TODO: delete after debug
+        if len(entries) == 3:
+          return entries
       start = i
   
   return entries 
@@ -73,4 +112,9 @@ def main(changelog):
   
   entries = parse_changelog_entries(lines)
   
-  
+  for entry in entries:
+    print entry
+
+
+if __name__ == '__main__':
+  main(sys.argv[1])
